6e214ea1ef97f71549ae215007f255f41ec24354
Julian Tekook <tekook@users.noreply.github.com>
2021-09-28 15:59:02 +0200

    Add RelationFilter

---
+++ src/RelationFilter.php
@@ -0,0 +1,22 @@
+<?php
+
+
+namespace Tekook\Filterable;
+
+
+use Illuminate\Database\Eloquent\Relations\Relation;
+
+abstract class RelationFilter extends Filter
+{
+    /**
+     * @var \Illuminate\Database\Eloquent\Relations\Relation
+     */
+    protected Relation $builder;
+
+    public function apply(Relation $relation)
+    {
+        $this->builder = $relation;
+
+        $this->applyRequest();
+    }
+}
